a8008790d9ff3d02d11b3c3750fd57a758632b1f
CiegeX <cjjimmy193@yahoo.com>
2024-12-18 18:12:31 -0800

    Functionaility to calendar and Timer

---
+++ Momentum/Schedule/CountdownTimer.swift
@@ -0,0 +1,18 @@
+//
+//  CountdownTimer.swift
+//  Momentum
+//
+//  Created by Auggie Dev on 12/18/24.
+//
+
+import SwiftUI
+
+struct CountdownTimer: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CountdownTimer()
+}
